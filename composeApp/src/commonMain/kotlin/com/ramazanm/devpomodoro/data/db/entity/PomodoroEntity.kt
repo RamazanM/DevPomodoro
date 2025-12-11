482fdbc65cbfc856afed44efc5be8a2535acc517
RamazanM <r.ramazanmutlu@gmail.com>
2025-12-11 21:47:57 +0300

    Implement Room persistence for tasks and pomodoros
    Introduced `TaskEntity`, `PomodoroEntity`, and `TaskWithPomodoros` along with `TaskDAO` and `PomodoroDAO` for database persistence. Added `RoomTaskRepository` implementing the updated `TaskRepository` interface to handle CRUD operations and fetch tasks with associated pomodoro sessions. Also updated `TaskDTO` and `PomodoroDTO` to support entity mapping and introduced `TaskWithPomodorosDTO`.

---
+++ composeApp/src/commonMain/kotlin/com/ramazanm/devpomodoro/data/db/entity/PomodoroEntity.kt
@@ -0,0 +1,28 @@
+package com.ramazanm.devpomodoro.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ramazanm.devpomodoro.data.dto.PomodoroDTO
+import com.ramazanm.devpomodoro.data.dto.PomodoroStatus
+import com.ramazanm.devpomodoro.data.dto.PomodoroType
+
+@Entity
+data class PomodoroEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val type: PomodoroType,
+    val startTime: Long,
+    val endTime: Long,
+    val status: PomodoroStatus,
+    val duration: Long,
+    val taskId: Int
+){
+    fun toDTO(): PomodoroDTO = PomodoroDTO(
+        id = id,
+        type = type,
+        startTime = startTime,
+        endTime = endTime,
+        status = status,
+        duration = duration,
+        taskId = taskId
+    )
+}
--- composeApp/src/commonMain/kotlin/com/ramazanm/devpomodoro/data/repository/TaskRepository.kt
+++ composeApp/src/commonMain/kotlin/com/ramazanm/devpomodoro/data/repository/TaskRepository.kt
@@ -1,12 +1,16 @@
 package com.ramazanm.devpomodoro.data.repository
 
+import com.ramazanm.devpomodoro.data.dto.PomodoroDTO
 import com.ramazanm.devpomodoro.data.dto.TaskDTO
-import com.ramazanm.devpomodoro.data.dto.TaskSourceDTO
+import com.ramazanm.devpomodoro.data.dto.TaskWithPomodorosDTO
 
-abstract class TaskRepository(taskSourceDTO: TaskSourceDTO){
-    abstract suspend fun getTasks(): List<TaskDTO>
-    abstract suspend fun getTask(id: Int): TaskDTO
+abstract class TaskRepository() {
+    abstract suspend fun getTask(id: Int): TaskDTO?
     abstract suspend fun addTask(taskDTO: TaskDTO)
     abstract suspend fun updateTask(taskDTO: TaskDTO)
     abstract suspend fun deleteTask(id: Int)
+    abstract suspend fun getTasksWithPomodoros(): List<TaskWithPomodorosDTO>
+    abstract suspend fun addPomodoro(pomodoroDTO: PomodoroDTO)
+    abstract suspend fun updatePomodoro(pomodoroDTO: PomodoroDTO)
+
 }
