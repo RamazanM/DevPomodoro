fdd0c6d98b204cefd6ee6e757042289e732d7025
RamazanM <r.ramazanmutlu@gmail.com>
2025-12-12 00:20:14 +0300

    Introduce TaskListViewModel for task management state
    Added `TaskListViewModel` to manage UI state for displaying tasks. It interacts with `TaskRepository` to fetch tasks and exposes a `TaskListState` flow, which includes `Loading`, `Success`, `Error`, and `Empty` states to represent the data loading lifecycle.

---
+++ composeApp/src/commonMain/kotlin/com/ramazanm/devpomodoro/presentation/TaskListViewModel.kt
@@ -0,0 +1,50 @@
+package com.ramazanm.devpomodoro.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ramazanm.devpomodoro.data.dto.TaskWithPomodorosDTO
+import com.ramazanm.devpomodoro.data.repository.TaskRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+open class TaskListState {
+    object Loading : TaskListState()
+    data class Success(val tasks: List<TaskWithPomodorosDTO>) : TaskListState()
+    data class Error(val message: String) : TaskListState()
+    object Empty : TaskListState()
+}
+
+class TaskListViewModel(val repository: TaskRepository) : ViewModel() {
+    private val _taskListState = MutableStateFlow<TaskListState>(TaskListState.Loading)
+    val taskListState: StateFlow<TaskListState> = _taskListState
+
+    init {
+        loadTasks()
+    }
+
+    fun loadTasks() {
+        viewModelScope.launch {
+            _taskListState.update { TaskListState.Loading }
+            try {
+                val tasks = repository.getTasksWithPomodoros()
+                if (tasks.isEmpty()) {
+                    _taskListState.update { TaskListState.Empty }
+                } else {
+                    _taskListState.update {
+                        TaskListState.Success(tasks)
+                    }
+                }
+            } catch (e: Exception) {
+                _taskListState.update {
+                    TaskListState.Error(e.message ?: "Unknown error")
+                }
+            }
+
+        }
+
+    }
+
+
+}
