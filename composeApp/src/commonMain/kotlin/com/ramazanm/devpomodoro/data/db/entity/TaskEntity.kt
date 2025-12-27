5dbecd779a3c8386c01398e23c500b737eb86a4b
RamazanM <r.ramazanmutlu@gmail.com>
2025-12-27 16:44:43 +0300

    feat: Set default values for TaskEntity properties

--- composeApp/src/commonMain/kotlin/com/ramazanm/devpomodoro/data/db/entity/TaskEntity.kt
+++ composeApp/src/commonMain/kotlin/com/ramazanm/devpomodoro/data/db/entity/TaskEntity.kt
@@ -4,17 +4,18 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.ramazanm.devpomodoro.data.dto.TaskDTO
 import com.ramazanm.devpomodoro.data.dto.TaskSourceType
+import kotlin.time.Clock
 
 @Entity
 data class TaskEntity(
     @PrimaryKey(autoGenerate = true) val id: Int? = 0,
-    val title: String,
-    val description: String,
-    val status: String,
-    val source: TaskSourceType,
-    val startDate: Long,
-    val endDate: Long,
-    val priority: Int,
+    val title: String="",
+    val description: String="",
+    val status: String="",
+    val source: TaskSourceType= TaskSourceType.LOCAL,
+    val startDate: Long= Clock.System.now().epochSeconds,
+    val endDate: Long=Clock.System.now().epochSeconds,
+    val priority: Int=1,
 ){
     fun toDTO(): TaskDTO = TaskDTO(
         id = id,
